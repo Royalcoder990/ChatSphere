35793e048bad5e30261f129a913fadb6862a2775
Rishabh Ralli <103279123+Royalcoder990@users.noreply.github.com>
2023-08-29 00:17:49 +0530

    Further Built Login

--- chatsphere/src/components/account/LoginDialog.jsx
+++ chatsphere/src/components/account/LoginDialog.jsx
@@ -1,10 +1,12 @@
+import LoginForm from "./LoginForm";
 import TypedText from "./TypedText";
-
+import "./LoginDialog.css"
 
 const LoginDialog=()=>{
     return(
-        <div>
-            <TypedText/>
+        <div className="LD">
+            <div className="LF"><LoginForm/></div>
+            <div className="TT"><TypedText/></div>
         </div>
     )
 }
